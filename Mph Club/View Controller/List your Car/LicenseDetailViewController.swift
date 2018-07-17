256c0d0f395dc4220446f3b176e20850b3c716ab
Alex Cruz <alexcruz@davids-5k-imac.hsd1.fl.comcast.net>
2018-07-17 16:42:42 -0400

    Adding additional views to listing

---
+++ Mph Club/View Controller/List your Car/LicenseDetailViewController.swift
@@ -0,0 +1,47 @@
+//
+//  LicenseDetailViewController.swift
+//  Mph Club
+//
+//  Created by Alex Cruz on 7/17/18.
+//  Copyright © 2018 Mph Club. All rights reserved.
+//
+
+import UIKit
+
+class LicenseDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let button1 = UIBarButtonItem(image: UIImage(named: "backArrow"), style: .plain, target: self, action: #selector(LicenseDetailViewController.close))
+        button1.tintColor = UIColor.black
+        self.navigationItem.leftBarButtonItem  = button1
+        
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = UIColor.clear
+        
+        self.navigationController?.navigationBar.backgroundColor = UIColor.clear
+    }
+    
+    @objc func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
