a87f5d20fb37d77703db0d7483ef59b524e69ece
behzad ardeh <>
2018-10-11 18:46:41 +0330

    add palette color

--- Mph Club/Custom Classes/Helpers.swift
+++ Mph Club/Custom Classes/Helpers.swift
@@ -9,17 +9,7 @@ func generateRandomData() -> [[UIColor]] {
     }
 }
 
-extension UIColor {
-    
-    class func randomColor() -> UIColor {
 
-        let hue = CGFloat(arc4random() % 100) / 100
-        let saturation = CGFloat(arc4random() % 100) / 100
-        let brightness = CGFloat(arc4random() % 100) / 100
-
-        return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1.0)
-    }
-}
 
 
 // Alerts
