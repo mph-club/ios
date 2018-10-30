b3ce8f6b60506a4ac7c6075fd1e859b8f5e2ffdb
behzad ardeh <>
2018-10-30 21:23:06 +0330

    add trips module

---
+++ Mph Club/Modules/Trips/Controllers/TripsViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TripsViewController.swift
+//  Mph Club
+//
+//  Created by behzad ardeh on 10/30/18.
+//  Copyright © 2018 Mph Club. All rights reserved.
+//
+
+import UIKit
+
+final class TripsViewController: UIViewController {
+    // ===============
+    // MARK: - Outlets
+    // ===============
+    
+    // ==================
+    // MARK: - Properties
+    // ==================
+}
+
+// =======================
+// MARK: - View Controller
+// =======================
+
+// MARK: Life Cycle
+extension TripsViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+}
+
+// ===============
+// MARK: - Actions
+// ===============
+private extension TripsViewController {}
+
+// ===============
+// MARK: - Methods
+// ===============
+private extension TripsViewController {}
