cdb3b64fb32f8e9bf2f61acaff9e37c4578f33c7
behzad ardeh <>
2018-10-31 13:19:18 +0330

    add account view controller

---
+++ Mph Club/Modules/Account/Controllers/AccountViewController.swift
@@ -0,0 +1,143 @@
+//
+//  AccountViewController.swift
+//  Mph Club
+//
+//  Created by Alex Cruz on 9/18/18.
+//  Copyright © 2018 Mph Club. All rights reserved.
+//
+
+import UIKit
+import AWSCognitoIdentityProvider
+
+final class AccountViewController: UIViewController {
+    // =============
+    // MARK: - Enums
+    // =============
+    private enum CellItems: String, CaseIterable {
+        case contactInfo = "Contact info"
+        case profilePhoto = "Profile photo"
+        case paymentInfo = "Payment info"
+        case transactionHistory = "Transaction history"
+        case faq = "FAQ"
+        case contactSupport = "Contact support"
+    }
+    
+    // ===============
+    // MARK: - Outlets
+    // ===============
+    
+    // MARK: Table View
+    @IBOutlet private weak var tableView: UITableView! {
+        didSet {
+            // Register table View cell
+            registerTableViewCell()
+        }
+    }
+    
+    // ==================
+    // MARK: - Properties
+    // ==================
+    
+    // MARK: Private
+    private var response: AWSCognitoIdentityUserGetDetailsResponse?
+    private var user: AWSCognitoIdentityUser?
+    private var pool: AWSCognitoIdentityUserPool?
+    
+    // MARK: Override
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
+
+// =======================
+// MARK: - View Controller
+// =======================
+
+// MARK: Life Cycle
+extension AccountViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //
+        pool = AWSCognitoIdentityUserPool(forKey: AWSCognitoUserPoolsSignInProviderKey)
+        //
+        if self.user == nil {
+            self.user = self.pool?.currentUser()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // hide navigation bar
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+}
+
+// ===============
+// MARK: - Actions
+// ===============
+private extension AccountViewController {
+    @IBAction func signOut(_ sender: UIButton) {
+        //
+        tabBarController?.selectedIndex = 0
+        //
+        user?.signOut()
+        //
+        refresh()
+    }
+}
+
+// ===============
+// MARK: - Methods
+// ===============
+private extension AccountViewController {
+    func registerTableViewCell() {
+        tableView.register(AccountTableViewCell.self)
+    }
+    
+    func refresh() {
+        user?.getDetails().continueOnSuccessWith { task in
+            DispatchQueue.main.async {
+                self.response = task.result
+            }
+            return nil
+        }
+    }
+}
+
+// ===================
+// MARK: - Scroll View
+// ===================
+
+// MARK: Delegate
+extension AccountViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.y <= 0 {
+            tableView.backgroundColor = .black
+        } else {
+            tableView.backgroundColor = .white
+        }
+    }
+}
+
+// ==================
+// MARK: - Table View
+// ==================
+
+// MARK: Data Source
+extension AccountViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return CellItems.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: AccountTableViewCell = tableView.dequeueReusableCell(for: indexPath)
+        cell.setContent(CellItems.allCases[indexPath.row].rawValue)
+        return cell
+    }
+}
+
+extension AccountViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 75
+    }
+}
