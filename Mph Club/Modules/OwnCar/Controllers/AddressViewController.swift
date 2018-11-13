2e77ad526a77676edca69cffccffaf7c8d886eaa
behzad ardeh <>
2018-11-13 16:57:27 +0330

    edit address view controller ui

---
+++ Mph Club/Modules/OwnCar/Controllers/AddressViewController.swift
@@ -0,0 +1,155 @@
+//
+//  AddressViewController.swift
+//  Mph Club
+//
+//  Created by Alex Cruz on 7/4/18.
+//  Copyright © 2018 Mph Club. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import Contacts
+
+final class AddressViewController: UIViewController {
+    // ===============
+    // MARK: - Outlets
+    // ===============
+    
+    // MARK: Text Field
+    @IBOutlet private weak var addressTextField: MphTextField!
+    @IBOutlet private weak var cityAndStateTextField: MphTextField!
+    @IBOutlet private weak var placeTextField: MphTextField!
+    
+    // MARK: Label
+    @IBOutlet private weak var cityAndStateLabel: UILabel!
+    @IBOutlet private weak var placesLabel: UILabel!
+    
+    // MARK: Button
+    @IBOutlet private weak var nextButton: UIButton!
+    
+    // ==================
+    // MARK: - Properties
+    // ==================
+    
+    // MARK: Object sent over in segue
+    private var location: CLPlacemark?
+    
+    // MARK: Object to pass to next view controller
+    private var carLocation = [String: String]()
+}
+
+// =======================
+// MARK: - View Controller
+// =======================
+
+// MARK: Life Cycle
+extension AddressViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        nextButton.backgroundColor = UIColor.lightGray
+        setUpTextField(color: UIColor.lightGray.cgColor)
+        
+        cityAndStateLabel.textColor = UIColor.lightGray
+        placesLabel.textColor = UIColor.lightGray
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if location != nil {
+            updateForm()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        addressTextField.isUserInteractionEnabled = true
+    }
+}
+
+// MARK: Navigation
+extension AddressViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let carEligibilityViewController = segue.destination as? CarEligibilityViewController {
+            carEligibilityViewController.location = carLocation
+        }
+    }
+}
+
+// ===============
+// MARK: - Actions
+// ===============
+private extension AddressViewController {
+    @IBAction func nextButton(_ sender: UIButton) {
+        if !(addressTextField.text?.isEmpty ?? true) ||
+            !(cityAndStateTextField.text?.isEmpty ?? true) ||
+            !(placeTextField.text?.isEmpty ?? true) {
+            carLocation["address"] = addressTextField.text
+            carLocation["city"] = location?.postalAddress?.city
+            carLocation["state"] = location?.postalAddress?.state
+            carLocation["place"] = placeTextField.text
+        }
+        performSegue(withIdentifier: "goToEnterCar", sender: nil)
+    }
+    
+    @IBAction func backButton(_ sender: UIBarButtonItem) {
+        let fireAction = fireActionSheet(title: "Before you close",
+                                         message: "If you proceed with this action, you'll have to start from the beginning.")
+        
+        fireAction.addAction(UIAlertAction(title: "Confirm", style: .default) { [weak self] _ in
+            self?.dismiss(animated: true, completion: nil)
+        })
+        
+        fireAction.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(fireAction, animated: true)
+    }
+    
+    @IBAction func unwindToMenu(segue: UIStoryboardSegue) {}
+    
+    @IBAction func unwindToAddress(segue: UIStoryboardSegue) {}
+}
+
+// ===============
+// MARK: - Methods
+// ===============
+private extension AddressViewController {
+    func updateForm() {
+        nextButton.backgroundColor = UIColor.black
+        setUpTextField(color: UIColor.black.cgColor)
+        
+        addressTextField.text = location?.postalAddress?.street
+        
+        guard let city = location?.locality else { return }
+        guard let state = location?.administrativeArea else { return }
+        
+        cityAndStateTextField.text = "\(String(describing: city)), \(String(describing: state))"
+        
+        if location?.postalAddress?.street != location?.name {
+            self.placeTextField.text = location?.name
+        } else {
+            self.placeTextField.text = ""
+        }
+    }
+    
+    func setUpTextField(color: CGColor) {
+        cityAndStateTextField.setBottomSingleBorder(color: color)
+        placeTextField.setBottomSingleBorder(color: color)
+        
+        cityAndStateLabel.textColor = UIColor.black
+        placesLabel.textColor = UIColor.black
+    }
+}
+
+// ==================
+// MARK: - Text Field
+// ==================
+
+// MARK: Delegate
+extension AddressViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        addressTextField.isUserInteractionEnabled = false
+        performSegue(withIdentifier: "goToSearchTable", sender: nil)
+    }
+}
